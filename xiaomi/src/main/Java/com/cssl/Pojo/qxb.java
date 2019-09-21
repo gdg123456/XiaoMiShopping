a4e574575060ad68248b553831235c3020d25ff1
daixinyuan <55432480+daixinyuan@users.noreply.github.com>
2019-09-21 17:42:59 +0800

    新加了5个表

---
+++ xiaomi/src/main/Java/com/cssl/Pojo/qxb.java
@@ -0,0 +1,18 @@
+package com.cssl.Pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class qxb  implements Serializable {//区县表
+
+    private int Qx_id;//区县id
+    private int Qx_name;//区县名称
+}
