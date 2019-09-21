be7ac6e7698bf64dd5b48e83df73f30b6bd58c89
aasongbo <2677242270@qq.com>
2019-09-21 16:52:23 +0800

    小米商城正式项目结构1.0

---
+++ xiaomi/src/main/Java/com/cssl/Dao/UserDao.java
@@ -0,0 +1,26 @@
+package com.cssl.Dao;
+
+/**
+ * 用户登录，注册，退出
+ */
+public interface UserDao {
+
+    /**
+     * 登录
+     * @return
+     */
+    public int denglu();
+
+    /**
+     * 注册
+     * @return
+     */
+    public int zhuche();
+
+    /**
+     * 注册验证
+     * @return
+     */
+    public int zhucheyz(String admin);
+
+}
